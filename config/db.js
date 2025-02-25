468d3b7a1b0e35230ef0ec08166eb3c501740b5d
Verrel <verrelangkasa20@gmail.com>
2025-02-26 00:11:01 +0700

    update app

--- config/db.js
+++ config/db.js
@@ -1,16 +1,6 @@
-const mysql = require("mysql2");
-require("dotenv").config();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-});
+dotenv.config();
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to MySQL database!");
-});
-
-module.exports = db;
+const 
