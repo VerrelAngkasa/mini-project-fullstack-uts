6303a88ae7d93cee7714f9d2b8ad026083de77c9
Verrel <verrelangkasa20@gmail.com>
2025-03-04 00:07:12 +0700

    Update model schema for task and user

--- models/taskModel.js
+++ models/taskModel.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
+// Task Collection in MongoDB
 const taskCollection = new mongoose.Schema({
   title: { type: String, required: true },
   category: { type: String, required: true },
   deadline: { type: Date, required: true },
-  status: { type: String, default: 'Belum Selesai' },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'userModel' },
+  status: { type: String, enum: ["pending", "progress", "done"], default: "pending" },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'userModel', required: true },
 });
 
 module.exports = mongoose.model('taskModel', taskCollection);
--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
+// User Collection in MongoDB
 const userCollection = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true }
